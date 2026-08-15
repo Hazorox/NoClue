378283ca01236372525e9d0a41ba022838c5987e
Hamza Awad <hamzaawad8899@gmail.com>
2026-08-15 18:35:06 +0300

    Add initial autoload game script

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,11 +1,6 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var room :=1
+var rooms_to_win :=12
+var finished :=false
+@onready var player:CharacterBody2D = get_tree().get_first_node_in_group("player")
