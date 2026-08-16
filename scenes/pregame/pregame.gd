ea625ba0487474918a6c324d69218b6ce8f4e778
Hamza Awad <hamzaawad8899@gmail.com>
2026-08-16 10:50:37 +0300

    modify main menu

--- scenes/pregame/pregame.gd
+++ scenes/pregame/pregame.gd
@@ -2,9 +2,8 @@ extends Node
 @onready var scoreLabel:Label = $contents/scoreControl/score
 @onready var increaseScore:Button = $"contents/scoreControl/buttons/plus"
 @onready var decreaseScore:Button = $"contents/scoreControl/buttons/minus"
-@onready var verdict:Label = $contents/verdict
-@onready var verdicts = ["SUCKER, BE STRONGER!!","That's too many T-T ( <20 )"]
-
+@onready var verdicts = ["SUCKER, BE STRONGER!!","Too many T-T <20"]
+@onready var start = $contents/start
 func _ready()->void:
 	scoreLabel.text = str(game.rooms_to_win)
 
@@ -17,14 +16,10 @@ func _on_plus_pressed() -> void:
 
 func _on_start_pressed() -> void:
 	if (int(scoreLabel.text) > 20):
-		verdict.text = verdicts[1]
-		verdict.visible=true
+		start.text = verdicts[1]
 		return
 	elif (int(scoreLabel.text)<5):
-		verdict.text = verdicts[0]
-		verdict.visible=true
+		start.text = verdicts[0]
 		return
-	else:
-		verdict.visible=false
 	game.rooms_to_win = int(scoreLabel.text)
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
