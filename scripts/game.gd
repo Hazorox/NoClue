d8e885d2b429a87d9ad7956404ed3ffed5002bff
Hamza Awad <hamzaawad8899@gmail.com>
2026-08-16 10:17:15 +0300

    fix error of player fetching

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,17 +3,12 @@ var finished :=false
 var rooms_to_win :=7
 var room :=1
 var correct_door:int = 0
-@onready var player:CharacterBody2D = get_tree().get_first_node_in_group("player")
-@onready var sound:AudioStreamPlayer = get_tree().get_first_node_in_group("rickroll")
-func _process(_delta:float)->void:
-	if room==rooms_to_win:
-		sound.play()
 
 func advance(success:bool)->void:
-	get_tree().paused=true
-	
-	
-	get_tree().paused=false
+	var player:CharacterBody2D = get_tree().get_first_node_in_group("player")
+	# Centering player once again
+	player.global_position.x = 160
+	player.global_position.y = 120
 	if success:
 		room+=1
 	else:
