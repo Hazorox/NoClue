214270168a0e0a1c641e6eb6ceb0bd65a4f06a38
Hamza Awad <hamzaawad8899@gmail.com>
2026-08-16 10:51:11 +0300

    Fix interactability issues

--- scenes/interactable.gd
+++ scenes/interactable.gd
@@ -6,8 +6,8 @@ func _ready() -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("interactable"):
-		DialogueManager.tqueue("Meow! I'm a talking cat.")
+		DialogueManager.tqueue("Meow! ".repeat(game.correct_door))
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("interactable"):
-		DialogueManager.tqueue("Meow! I'm a talking cat.")
+		DialogueManager.tqueue("Meow! ".repeat(game.correct_door))
