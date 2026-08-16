7bf2cdbf804f27e5c1c31b92a3801d45fb7e6e82
Hamza Awad <hamzaawad8899@gmail.com>
2026-08-16 10:51:51 +0300

    Randomly generate first door

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,7 +2,7 @@ extends Node
 var finished :=false
 var rooms_to_win :=7
 var room :=1
-var correct_door:int = 0
+var correct_door:int = RandomNumberGenerator.new().randi_range(1,5)
 
 func advance(success:bool)->void:
 	var player:CharacterBody2D = get_tree().get_first_node_in_group("player")
